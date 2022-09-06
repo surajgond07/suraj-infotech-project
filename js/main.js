54e556e4090a6960d10c3c9f27e75512d5200a2d
Suraj Gond <surajgond07@gmail.com>
2022-09-06 13:39:43 +0530

    Change JS toggle the nav menu code.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
-const collapsibles = document.querySelectorAll(".collapsible");
-collapsibles.forEach((item) =>
-    item.addEventListener("click", function() {
-        this.classList.toggle("collapsible--expanded");
-    })
-);
+// const collapsibles = document.querySelectorAll(".collapsible");
+// collapsibles.forEach((item) =>
+//     item.addEventListener("click", function() {
+//         this.classList.toggle("collapsible--expanded");
+//     })
+// );
+
+
+
+// const navToggler = document.querySelector("nav > svg");
+// const navMenu = document.querySelector("nav > ul");
+
+// navToggler.addEventListener("click", function() {
+//     navMenu.classList.toggle("collapsible–expanded");
+//     if (navMenu.classList.contains("collapsible__content") === true) {
+//         navMenu.classList.remove("collapsible__content");
+//     } else {
+//         navMenu.classList.add("collapsible__content");
+//     }
+// });
+
+document.querySelector('nav > svg').addEventListener('click', () => {
+    document.querySelector('nav > ul').classList.toggle('collapsible__content')
+})
