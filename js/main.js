45f0cc3b747a9503d29c3a9499e033494ef95079
Suraj Gond <surajgond07@gmail.com>
2022-09-07 18:11:14 +0530

    Modify main.js to enable animations & togglers.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 const navToggler = document.querySelector("nav > svg");
-const navMenu = document.querySelector("nav > ul");
 
 const collapsibles = document.querySelectorAll(".collapsible.footer__section");
 
 // Click event for navbar navigation menu
-navToggler.addEventListener("click", () => {
-    navMenu.classList.toggle("collapsible__content");
+navToggler.addEventListener("click", (e) => {
+    e.currentTarget.parentElement.classList.toggle("collapsible--expanded");
 });
 
 // Click events for footer navigation menus
 collapsibles.forEach((item) => {
     item.addEventListener("click", (e) => {
-        e.currentTarget.querySelector("div:nth-child(2)").classList.toggle("collapsible__content");
+        e.currentTarget.classList.toggle("collapsible--expanded");
     });
 });
