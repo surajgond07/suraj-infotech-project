f4a2768a04c6bec6da9492335e86a6791f5a37cd
Suraj Gond <surajgond07@gmail.com>
2023-01-11 20:12:50 +0530

    Scroll to top adding

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+// jquery scroll up function
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if (this.scrollY > 20) {
+            $('.nav').addClass("sticky");
+        } else {
+            $('.nav').removeClass("sticky");
+        }
+        // scroll up btn 
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script 
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({ scrollTop: 0 });
+    });
+
+});
+
+
+// JavaScript toggler function
+
 const navToggler = document.querySelector("nav > svg");
 
 const collapsibles = document.querySelectorAll(".collapsible.footer__section");
